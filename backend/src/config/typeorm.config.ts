c237cd405b19d12573c989d53da240ca3fb6154c
Tofik Abdullayev <abdullayevtofik@gmail.com>
2021-09-19 00:59:06 +0400

    test db

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: "localhost",
   port: 5432,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "alphachem",
   entities: [__dirname + "/../**/*.entity.js"],
   synchronize: true,
