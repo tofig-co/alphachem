afbc3bd65187e4c3a836a60a2a632b819f5a0f3a
Tofik Abdullayev <abdullayevtofik@gmail.com>
2021-09-23 14:48:20 +0400

    increased file upload limit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as bodyParser from "body-parser";
+
 import * as helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-
+  app.use(bodyParser.json({ limit: "50mb" }));
+  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
   app.setGlobalPrefix("api");
   await app.listen(3000);
 }
