8ef0a21d17e00329532f0717a0a85f3bf870e987
Tofik Abdullayev <abdullayevtofik@gmail.com>
2021-09-23 14:35:59 +0400

    backend deploy

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -4,9 +4,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "postgres",
   host: "localhost",
   port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "alphachem",
+  username: "alphachem-user",
+  password: "tofik!547",
+  database: "alphachem-db",
   entities: [__dirname + "/../**/*.entity.js"],
   synchronize: true,
 };
